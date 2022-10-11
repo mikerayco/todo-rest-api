6ba5bf65e2f312539d23dc615905733a7968e82b
Mike Rayco <michaelangelorayco@gmail.com>
2022-10-11 21:43:43 +0800

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:18.04
+
+RUN apt-get update \
+    && apt-get install -y python3-pip python3-dev \
+    && cd /usr/local/bin \
+    && ln -s /usr/bin/python3 python \
+    && pip3 install --upgrade pip
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+CMD [ "python3", "app.py" ]
--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Resource: https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
-
 from IPython import embed
 from flask import Flask, jsonify, abort, make_response, request
 from flask import url_for
@@ -7,40 +5,38 @@ from flask_httpauth import HTTPBasicAuth
 
 
 app = Flask(__name__)
-
-
 auth = HTTPBasicAuth()
 
 
 @auth.get_password
 def get_password(username):
-    if username == 'miguel':
-        return 'python'
+    if username == "admin":
+        return "python"
     return None
 
 
 @auth.error_handler
 def unauthorized():
-    return make_response(jsonify({'error': 'unauthorized access'}), 401)
+    return make_response(jsonify({"error": "unauthorized access"}), 401)
 
 
 tasks = [
     {
-        'id': 1,
-        'title': 'Buy groceries',
-        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
+        "id": 1,
+        "title": "Buy groceries",
+        "description": "Milk, Cheese, Pizza, Fruit, Tylenol",
+        "done": False,
     },
     {
-        'id': 2,
-        'title': 'Learn Python',
-        'description': 'Need to find a good Python tutorial on the web',
-        'done': False
-    }
+        "id": 2,
+        "title": "Learn Python",
+        "description": "Need to find a good Python tutorial on the web",
+        "done": False,
+    },
 ]
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Hello, World!"
 
@@ -49,15 +45,16 @@ def index():
 # def get_tasks():
 #     return jsonify({'tasks': tasks})
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+
+@app.route("/todo/api/v1.0/tasks", methods=["GET"])
 @auth.login_required()
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+    return jsonify({"tasks": [make_public_task(task) for task in tasks]})
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["GET"])
 def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = [task for task in tasks if task["id"] == task_id]
     if not task:
         abort(404)
     return jsonify(task)
@@ -65,72 +62,72 @@ def get_task(task_id):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@app.route("/todo/api/v1.0/tasks", methods=["POST"])
 def create_task():
 
-    if not request.json or 'title' not in request.json:
+    if not request.json or "title" not in request.json:
         abort(400)
     task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ''),
-        'done': False
+        "id": tasks[-1]["id"] + 1,
+        "title": request.json["title"],
+        "description": request.json.get("description", ""),
+        "done": False,
     }
     tasks.append(task)
-    return jsonify({'task': task}), 201
+    return jsonify({"task": task}), 201
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = [task for task in tasks if task["id"] == task_id]
     if not task:
         abort(404)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != str:
+    if "title" in request.json and type(request.json["title"]) != str:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not str:
+    if "description" in request.json and type(request.json["description"]) is not str:
         abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    task[0]["title"] = request.json.get("title", task[0]["title"])
+    task[0]["description"] = request.json.get("description", task[0]["description"])
+    task[0]["done"] = request.json.get("done", task[0]["done"])
+    return jsonify({"task": task[0]})
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = [task for task in tasks if task["id"] == task_id]
     if not task:
         abort(404)
     tasks.remove(task[0])
-    return jsonify({'result': True})
+    return jsonify({"result": True})
 
 
 def make_public_task(task):
     new_task = {}
     for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        if field == "id":
+            new_task["uri"] = url_for("get_task", task_id=task["id"], _external=True)
         else:
             new_task[field] = task[field]
     return new_task
 
 
 def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
+    func = request.environ.get("werkzeug.server.shutdown")
     if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
+        raise RuntimeError("Not running with the Werkzeug Server")
     func()
 
 
-@app.route('/shutdown', methods=['GET'])
+@app.route("/shutdown", methods=["GET"])
 def shutdown():
     shutdown_server()
-    return 'Server shutting down...'
+    return "Server shutting down..."
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080", debug=True)
